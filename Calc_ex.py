c1b39fec6aa93af9d9d2449123950b3a69bcb96f
sotoro.kun <sotoro.kun@gmail.com>
2026-03-18 18:26:38 -0700

    updated my code

--- Calc_ex.py
+++ Calc_ex.py
@@ -1,1 +1,12 @@
-print("hello world ")
+def factorial_recursive(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    
+    if n == 0 or n == 1:
+        return 1
+        
+    return n * factorial_recursive(n - 1)
+
+number = input("Enter a number: ")
+print("--- Calculating Factorial for" + number+"!---")
+print(factorial_recursive(number))
